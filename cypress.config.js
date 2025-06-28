0134922a4fd3afa4b86b55eacde636f02469ac66
shwetabh <shwetabh.srivastava@gmail.com>
2025-06-28 12:53:25 +0530

    sss

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const cucumber =require("cypress-cucumber-preprocessor").default
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+
+//specPattern:"cypress/e2e/feature/*.feature",
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+
+      on('file:preprocessor',cucumber())
+    },
+specPattern:"cypress/e2e/feature/*.feature"
+
+  },
+});
